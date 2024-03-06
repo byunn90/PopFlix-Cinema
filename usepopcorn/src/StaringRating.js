ce497f8dbf324e7b41606b92266fef69d7f1b62c
byunn90 <kayhanturk90@Live.com>
2024-03-06 23:18:11 +1100

    got the star working again

--- usepopcorn/src/StaringRating.js
+++ usepopcorn/src/StaringRating.js
@@ -10,30 +10,36 @@ const starContainerStyle = {
   display: "flex",
 };
 
-const textStyle = {
-  lineHeigh: "1",
-  margin: "0",
-};
-
-export default function StarRating({ maxRating = 5 }) {
+export default function StarRating({
+  maxRating = 5,
+  color = "#fcc419",
+  size = 48,
+}) {
   const [rating, setRating] = useState(0);
   const [tempRating, setTempRating] = useState(0);
 
   function handleRating(onRate) {
-    // setRating(onRate);
-    setRating(onRate === 0 ? setRating(0) : onRate);
+    setRating(onRate === 1 ? setTempRating(0) : onRate);
   }
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    color,
+    fontSize: `${size / 1.5}px`,
+  };
 
   return (
     <div style={containerStyle}>
       <div style={starContainerStyle}>
-        {Array.from({ length: maxRating }, (_, i) => (
+        {Array.from({ length: maxRating }, (_, i = 0) => (
           <Star
             key={i}
             onRate={() => handleRating(i + 1)}
             onHoverIn={() => setTempRating(i + 1)}
-            onHoverOut={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(rating)}
             full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            color={color}
+            size={size}
           />
         ))}
         <p style={textStyle}>{tempRating || rating || ""}</p>
@@ -42,15 +48,14 @@ export default function StarRating({ maxRating = 5 }) {
   );
 }
 
-const starStyle = {
-  width: "48px",
-  height: "48px",
-  display: "block",
-  cursor: "pointer",
-  color: "gold",
-};
-
-function Star({ onRate, full, onHoverIn, onHoverOut }) {
+function Star({ onRate, full, onHoverIn, onHoverOut, size, color }) {
+  const starStyle = {
+    width: `${size}px`,
+    height: `${size}px`,
+    display: "block",
+    cursor: "pointer",
+    color: `${color}`,
+  };
   return (
     <span
       style={starStyle}
@@ -62,8 +67,8 @@ function Star({ onRate, full, onHoverIn, onHoverOut }) {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="#ffd700"
-          stroke="#ffd700"
+          fill={color}
+          stroke={color}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
