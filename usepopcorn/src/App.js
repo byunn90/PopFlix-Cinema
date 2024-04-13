f6e24a7dd80e097da7c07a60b2aaa93f522c0ce6
byunn90 <kayhanturk90@Live.com>
2024-04-13 20:51:32 +1000

    poster on right hand side

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -273,12 +273,43 @@ function Movie({ movies, onSelectMovie }) {
 }
 
 function MovieDetails({ selectedId, onCloseMovie }) {
+  const [movie, setMovie] = useState({});
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: realeased,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+  } = movie;
+  console.log(title, year);
+
+  useEffect(function () {
+    async function getMovieDetails() {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+      );
+      const data = await res.json();
+      console.log(data);
+      setMovie(data);
+    }
+    getMovieDetails();
+  }, []);
+
   return (
     <div className="details">
-      <button className="btn-back" onClick={onCloseMovie}>
-        &larr;
-        {selectedId}
-      </button>
+      <header>
+        <button className="btn-back" onClick={onCloseMovie}>
+          &larr;
+          {selectedId}
+        </button>
+        <img src={poster} alt={`Poster of ${movie} movie`} />
+      </header>
     </div>
   );
 }
