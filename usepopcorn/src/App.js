3e9a9dab17232e9165caab8d36f0fab84e13fcc0
byunn90 <kayhanturk90@Live.com>
2024-05-14 14:56:17 +1000

    useref

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -274,6 +274,15 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   const [isLoading, setIsLoading] = useState(false);
   const [userRating, setUserRating] = useState("");
 
+  const countRef = useRef(0);
+
+  useEffect(
+    function () {
+      if (userRating) countRef.current++;
+    },
+    [userRating]
+  );
+
   const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
   // console.log(isWatched);
   const watchedUserRating = watched.find(
@@ -310,6 +319,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       imdbRating: Number(imdbRating),
       runtime: Number(runtime.split("").at(0)),
       userRating,
+      countRatingDecisions: countRef.current,
     };
     onAddWatched(newWatchedMovie);
     // onCloseMovie();
